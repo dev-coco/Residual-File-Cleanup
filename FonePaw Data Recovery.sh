ffcf3388a00e7d773d2596423ced328dc36acb9a
Raz1ner <63853884+dev-coco@users.noreply.github.com>
2023-03-12 08:44:56 -0400

    Delete FonePaw Data Recovery.sh

--- FonePaw Data Recovery.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# com.fonepaw.fonepaw-data-recovery
-
-rm -rf /Applications/FonePaw\ Data\ Recovery.app
-rm -rf ~/Library/Preferences/com.fonepaw.fonepaw-data-recovery.plist
-rm -rf ~/Library/Saved\ Application\ State/com.fonepaw.fonepaw-data-recovery.savedState
-rm -rf ~/Library/Application\ Support/FonePaw
-
-echo done
